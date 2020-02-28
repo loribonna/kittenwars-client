08721c0a5c9e3b364e84ea939ed0d01db32cc884
Lorenzo Bonicelli <lorenzobonicelli@libero.it>
2020-02-28 17:01:49 +0100

    Add base url getter

--- src/helpers/statics.ts
+++ src/helpers/statics.ts
@@ -1,5 +1,12 @@
-export const BASE_URI='http://localhost:3000';
-export const KITTENS_URI = BASE_URI + '/kittens/';
+export const getBaseUrl = () => {
+    const port = window.location.port;
+    const hostname = window.location.hostname;
+    console.log(hostname, port);
+
+    return "https://"+hostname+":"+port;
+}
+
+export const BASE_URI=getBaseUrl(); // 'https://kittenwars-awm.herokuapp.com:';export const KITTENS_URI = BASE_URI + '/kittens/';
 export const VOTE_URI = BASE_URI + '/vote';
 export const LOGIN_URI = BASE_URI + '/app/login';
 export const LOGOUT_URI = BASE_URI + '/auth/logout'
