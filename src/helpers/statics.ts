f921d51dd4a64c15f1e88e992660c49d0a9a4b47
Lorenzo Bonicelli <lorenzobonicelli@libero.it>
2020-02-28 17:48:39 +0100

    Fix hostname

--- src/helpers/statics.ts
+++ src/helpers/statics.ts
@@ -1,14 +1,12 @@
 export const getBaseUrl = () => {
-    const port = window.location.port;
-    const hostname = window.location.hostname;
-    console.log(hostname, port);
+	const hostname = window.location.hostname;
+	return 'http://' + hostname;
+};
 
-    return "https://"+hostname+":"+port;
-}
-
-export const BASE_URI=getBaseUrl(); // 'https://kittenwars-awm.herokuapp.com:';export const KITTENS_URI = BASE_URI + '/kittens/';
+export const BASE_URI = getBaseUrl();
+export const KITTENS_URI = BASE_URI + '/kittens/';
 export const VOTE_URI = BASE_URI + '/vote';
 export const LOGIN_URI = BASE_URI + '/app/login';
-export const LOGOUT_URI = BASE_URI + '/auth/logout'
+export const LOGOUT_URI = BASE_URI + '/auth/logout';
 export const DEFAULT_URI = BASE_URI + '/app/score';
 export const ADMIN_URI = BASE_URI + '/admin/';
